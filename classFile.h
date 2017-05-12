169af4fd4ca47b850eb36fb9732e3ab9364ac635
linjuntan <linjuntan@sensetime.com>
2017-05-12 16:41:26 +0800

    fix

--- classFile.h
+++ classFile.h
@@ -10,14 +10,10 @@ private:
 	u2 minor_version;
 	u2 major_version;
 	u2 constant_pool_count;
-<<<<<<< HEAD
 	constant_info* info;
 	u2 access_flags;
 	u2 this_class;
 	u2 spuer_class;
 	u2 interfaces_count;
-	
-=======
 
->>>>>>> 696d02f01bf5a12d9814c22365a1bb7387c8de3e
 };
